37175e57fb0d7fb530d390ce0c151a013b7d2df3
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-11-28 03:03:45 +0600

    add the article form client side

--- src/Admin/AllArticle.jsx
+++ src/Admin/AllArticle.jsx
@@ -1,38 +1,45 @@
+import { Button } from "flowbite-react";
 import useArticles from "../hooks/useArticles";
 
 const AllArticle = () => {
   const [articles, loading] = useArticles([]);
-  console.log(articles)
+  console.log(articles);
 
-  if(loading){
-    return <p>Loading</p>
+  if (loading) {
+    return <p>Loading</p>;
   }
   return (
     <div>
-      <h2>all Articles</h2>
-
-      {articles.map((item) =>(
-        <div 
-        key={item._id}
-        href="#"
-        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-      >
-        <img
-          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-          src={item.image}
-          alt=""
-        />
-        <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {item.article_title}
-          </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {item.description}
-          </p>
-        </div>
+      <h2 className="text-4xl font-semibold text-center text-red-400 mb-5">
+        All The Articles
+      </h2>
+      <div className="grid grid-cols-2 gap-5">
+        {articles.map((item) => (
+          <div key={item._id}>
+            <div className="max-w-2xl bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                <img className="rounded-t-lg w-full h-64" src={item.image} alt="" />
+              </a>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {item.article_title}
+                  </h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {item.description}
+                </p>
+                <div className="flex gap-2">
+                  <Button className="btn btn-sm">Approve</Button>
+                  <Button>Decline</Button>
+                  <Button>Delete</Button>
+                  <Button>Premium</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-      ) )}
-     
     </div>
   );
 };
--- src/pages/AddArticle/AddArticles.jsx
+++ src/pages/AddArticle/AddArticles.jsx
@@ -1,19 +1,65 @@
+import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+
+
+const image_hosting_key = import.meta.env.VITE_IMG_BB;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddArticles = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const imageFile = {image : data.image[0]}
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        'content-type' : 'multipart/form-data'
+      }
+    });
+    if(res.data.success){
+      const addItem = {
+        article_title : data.article_title,
+        category : data.category,
+        description: data.description,
+        publisher_name : data.publisher_name,
+        tags : data.tags,
+        image: res.data.data.display_url
+      }
+      const addRes = await axiosSecure.post('/allArticles', addItem);
+      console.log(addRes.data)
+      if(addRes.data.insertedId){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your work has been saved",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    }
+    console.log('with image url',res.data)
+  };
+
   return (
     <div className="min-h-screen">
       <h2 className="text-5xl font-medium text-center my-5 text-red-500">
         Add Article
       </h2>
       <div className="">
-        <form className=" p-10">
+        <form onSubmit={handleSubmit(onSubmit)} className=" p-10">
           <div className="grid gap-6 mb-6 md:grid-cols-2">
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Title
               </label>
               <input
+                {...register("article_title", { required: true })}
                 type="text"
-                name="title"
+                name="article_title"
                 id=""
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Title"
@@ -25,9 +71,10 @@ const AddArticles = () => {
                 Publisher Name
               </label>
               <input
+                {...register("publisher_name", { required: true })}
                 type="text"
                 id=""
-                name="PublisherName"
+                name="publisher_name"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Publisher Name"
                 required
@@ -38,6 +85,7 @@ const AddArticles = () => {
                 Tags
               </label>
               <input
+                {...register("tags", { required: true })}
                 type="text"
                 name="tags"
                 id=""
@@ -47,11 +95,29 @@ const AddArticles = () => {
               />
             </div>
 
+            <div className="">
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <span className="label-text">Category</span>
+              </label>
+              <select
+                defaultValue="default"
+                {...register("category", { required: true })}
+                className="select select-bordered  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option disabled value="default">
+                  Select a news category
+                </option>
+                <option value="publicationA">Publication A</option>
+                <option value="publicationB">Publication B</option>
+                <option value="publicationC">Publication C</option>
+              </select>
+            </div>
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Image File
               </label>
               <input
+                {...register("image", { required: true })}
                 className="block w-full mb-5 text-xs text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                 id="small_size"
                 type="file"
@@ -60,9 +126,11 @@ const AddArticles = () => {
           </div>
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-             Description
+              Description
             </label>
             <textarea
+            {...register("description", { required: true })}
+            name="description"
               id="message"
               rows="4"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -71,12 +139,12 @@ const AddArticles = () => {
           </div>
 
           <div className="flex justify-center items-center my-3">
-          <button
-            type="submit"
-            className="text-white   bg-red-500 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
+            <button
+              type="submit"
+              className="text-white   bg-red-500 hover:bg-red-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>

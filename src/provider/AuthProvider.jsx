3867f676cbb6e79f588de462f3fccb4a64bec422
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-11-28 00:22:06 +0600

    add custume hooks

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,6 +1,16 @@
 import { createContext, useEffect, useState } from "react";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -10,37 +20,46 @@ const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const axiosPublic = useAxiosPublic();
 
   const googleLogin = () => {
     setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-  const createUser = (email, password) =>{
-    setLoading(true)
-    return createUserWithEmailAndPassword(auth, email, password)
-  }
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-  const signIn =(email, password) =>{
-    return signInWithEmailAndPassword(auth, email, password)
-  }
-
-  const logOut =()=>{
-    setLoading(true)
-    return signOut(auth)
-  }
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
   useEffect(() => {
     const unSubScribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+      }
       console.log("currentUser", currentUser);
       setLoading(false);
     });
-    return () =>{
-        return unSubScribe()
-    }
+    return () => {
+      return unSubScribe();
+    };
   }, []);
 
   const handleUpdateProfile = (name, photo) => {
@@ -50,7 +69,6 @@ const AuthProvider = ({ children }) => {
     });
   };
 
-
   const authInfo = {
     user,
     loading,
@@ -58,8 +76,7 @@ const AuthProvider = ({ children }) => {
     signIn,
     logOut,
     handleUpdateProfile,
-    googleLogin
-
+    googleLogin,
   };
 
   return (

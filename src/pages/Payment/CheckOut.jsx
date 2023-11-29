06e5485927cd537dae23af79bf35cef928f89446
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-11-30 04:05:16 +0600

    add

---
+++ src/pages/Payment/CheckOut.jsx
@@ -0,0 +1,59 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { Button } from "flowbite-react";
+
+const CheckOut = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("[error]", error);
+    } else {
+      console.log("[PaymentMethod]", paymentMethod);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <Button className="mt-5" type="submit" disabled={!stripe}>
+          Pay
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckOut;
--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,12 +1,19 @@
+import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import CheckOut from "./CheckOut";
 
 
 
-const stripePromise = loadStripe('')
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_PK)
 const Payment = () => {
     return (
         <div className="min-h-screen">
             <h2 className="text-4xl font-semibold my-10 text-center text-black mb-14">Payment With Stripe</h2>
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOut></CheckOut>
+                </Elements>
+            </div>
         </div>
     );
 };
